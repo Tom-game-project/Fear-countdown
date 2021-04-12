4468e45f38310c833373f243b008940b1f600cbb
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2021-04-12 23:00:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function countdown(){
     var hour = Math.floor((sub-(day*24*60*60*1000))/(60*60*1000));
     var min = Math.floor((sub-((hour*60*60*1000)+(day*24*60*60*1000)))/(60*1000));
     var sec = Math.floor((sub-((min*60*1000)+(hour*60*60*1000)+(day*24*60*60*1000)))/1000);
-    var sen = `共通テストまで後${day}日${hour}時間${min}分${sec}秒`;
+    /*var sen = `共通テストまで後${day}日${hour}時間${min}分${sec}秒`;*/
+    var sen = '共通テストまで後'+day+'日'+hour+'時間'+min+'分'+sec+'秒';
     /*document.getElementById("couter").innerHTML = sen;*/
     $('#couter').text(sen)
     }
